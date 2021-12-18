add16eef85a4229ce35310c00b9817fbe58786e4
EverlastingDeath <68505580+EverlastingDeath@users.noreply.github.com>
2021-12-18 20:27:18 +0800

    Add files via upload

---
+++ video_stream_severpusher/myRtsp/cmake-build-debug/CMakeFiles/myRtsp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myRtsp.dir/main.cpp.o"
+  "myRtsp"
+  "myRtsp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myRtsp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
